6dcc633e22a46314c5932240089c9d9f9782d6d3
Aidan <aidansoftwaredev@gmail.com>
2020-04-19 21:12:56 +0100

    TS config and more examples

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-function combine(input1, input2, resultType) {
-    var result;
-    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
-        resultType === 'asNumber') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + ', ' + input2.toString();
-    }
-    return result;
-}
-var combinedAges = combine(30, 26, 'asNumber');
-var combinedStringAges = combine('30', '26', 'asNumber');
-var combinedNames = combine('Aidan', 'Max', 'asString');
-console.log(combinedAges);
-console.log(combinedStringAges);
-console.log(combinedNames);
+"use strict";
+var userName = 'Aidan L';
+console.log(userName);
---
+++ app.ts
@@ -0,0 +1,3 @@
+const userName = 'Aidan L';
+
+console.log(userName);
