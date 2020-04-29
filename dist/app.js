aaa8bc586e4f955627a3cadfb2f0b3005538a796
Aidan <aidansoftwaredev@gmail.com>
2020-04-29 21:16:57 +0100

    Adding CRA project

--- dist/app.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var userName = 'Aidan Low';
-console.log(userName);
---
+++ src/components/TextField.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  text: String
+}
+
+const TextField: React.FC<Props> = () => {
+  return (
+    <div>
+      <input />
+    </div>
+  )
+}
+
+export default TextField;
